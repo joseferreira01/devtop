75456c855117f83ad3743b1e6c8f31c3c38092e6
josé <joseifpb2015@gmail.com>
2021-09-13 17:51:19 -0300

    test

---
+++ src/main/java/br/com/socin/apigithub/controller/TesteControler.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.socin.apigithub.controller;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author jose
+ */
+@RestController
+@RequestMapping("/api")
+public class TesteControler {
+
+    @ApiOperation(value = "Teste de funcionamento")
+    @GetMapping()
+    public String test() {
+        return "API dev top no ar";
+    }
+}
