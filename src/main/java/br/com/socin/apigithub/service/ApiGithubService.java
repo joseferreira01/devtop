c9cf492baa88121b817c8db65e678495bdb803aa
josé <joseifpb2015@gmail.com>
2021-09-16 19:26:36 -0300

    he

--- src/main/java/br/com/socin/apigithub/service/ApiGithubService.java
+++ src/main/java/br/com/socin/apigithub/service/ApiGithubService.java
@@ -8,6 +8,7 @@ package br.com.socin.apigithub.service;
 import br.com.socin.apigithub.model.Metricas;
 import br.com.socin.apigithub.model.RemositoryGit;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 
 import org.springframework.stereotype.Service;
@@ -21,18 +22,30 @@ import reactor.core.publisher.Mono;
 @Service
 public class ApiGithubService {
 
+    @Value("${CLAENT_SECRETS}")
+    private String client_secrets;
+    @Value("${CLAENT_ID}")
+    private String client_id;
+
     @Autowired
     WebClient WebClient;
 
     public RemositoryGit find(Metricas metricas) {
-        // /search/repositories?q=language:TypeScript&sort=stars&page=1
+        System.err.println("id= "+client_id+ ",  chave= "+client_secrets);
+        
+         //search/repositories?q=language:TypeScript&sort=stars&page=1
         StringBuffer uri = new StringBuffer();
         uri.append("/search/repositories?q=language:");
         uri.append(metricas.getLanguage());
         uri.append("&sort=stars&page=");
         uri.append(metricas.getPage());
+        uri.append("&client_id=");
+        uri.append(client_id);
+        uri.append("&Client_secrets=");
+        uri.append(client_secrets);
         System.err.println("uri: search/repositories?q=language:TypeScript&sort=stars&page=1");
         System.err.println("uri sai " + uri.toString());
+        
         Mono<RemositoryGit> monoRemositoryGit = this.WebClient
                 //.method(HttpMethod.GET).uri("/users/joseferreira01")
                 .method(HttpMethod.GET).uri(uri.toString())
